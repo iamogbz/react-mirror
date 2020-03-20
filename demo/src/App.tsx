c2085535f2aa35ce54326627d57d63977c36e878
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-03-20 03:29:34 -0400

    chore: demo clock

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,24 +1,15 @@
 import * as React from "react";
 import { useMirror } from "react-mirror";
+import { Clock } from "./clock";
 import "./styles.css";
-
-function Clock(): JSX.Element {
-    const [time, setTime] = React.useState(Date.now());
-    React.useEffect(() => {
-        const timeout = setTimeout(() => setTime(Date.now()), 300);
-        return (): void => clearTimeout(timeout);
-    }, [time, setTime]);
-    return <div>{time}</div>;
-}
 
 export default function App(): JSX.Element {
     const [ref, reflection] = useMirror();
     return (
         <>
             <div className="App" ref={ref}>
-                <h1>Hello CodeSandbox</h1>
-                <h2>Start editing to see some magic happen!</h2>
-                <input />
+                <h1>React Mirror Demo</h1>
+                <input className="Input" placeholder="type something..." />
                 <Clock />
             </div>
             {reflection}
