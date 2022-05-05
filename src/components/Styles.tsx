ec4a466bff464042a2343835b38d1dc82575ea3c
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2022-05-05 17:41:14 -0400

    chore: remove unused style components

--- src/components/Styles.tsx
+++ src/components/Styles.tsx
@@ -1,40 +1,6 @@
 import * as React from "react";
 import { ElementProps } from "./Element";
 
-interface StylesProps {
-    sheetList: StyleSheetList;
-}
-
-export function Styles({ sheetList }: StylesProps) {
-    const sheets = React.useMemo(() => {
-        return Array.from(sheetList).map((sheet, i) => {
-            return <StyleSheet key={i} sheet={sheet} />;
-        });
-    }, [sheetList]);
-
-    return <>{sheets}</>;
-}
-
-interface StyleSheetProps {
-    sheet: CSSStyleSheet;
-}
-
-function StyleSheet({ sheet }: StyleSheetProps) {
-    const rules = React.useMemo(
-        () => Array.from(sheet.cssRules).map((rule) => rule.cssText),
-        [sheet],
-    );
-
-    return (
-        <Style
-            media={sheet.media.mediaText}
-            title={sheet.title ?? undefined}
-            type={sheet.type}
-            rules={rules}
-        />
-    );
-}
-
 interface StyleProps extends ElementProps<"style"> {
     rules: string[];
 }
