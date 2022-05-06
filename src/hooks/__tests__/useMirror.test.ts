faecf3cde29880857a5e5b5f2ce4934d6c1adad8
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2022-05-06 19:26:37 -0400

    test: add tests for useMirror hook

---
+++ src/hooks/__tests__/useMirror.test.ts
@@ -0,0 +1,29 @@
+import { act, renderHook } from "@testing-library/react";
+import { useMirror } from "../useMirror";
+
+describe("useMirror", () => {
+    it("renders mirror with props and target", async () => {
+        const props = {
+            frameProps: {},
+            mirrorProp: "",
+        };
+        const { result } = renderHook(() => useMirror(props));
+        expect(result.current[1]).toMatchInlineSnapshot(`
+            <Mirror
+              frameProps={Object {}}
+              mirrorProp=""
+            />
+        `);
+
+        const targetNode = document.createElement("div");
+        await act(async () => result.current[0](targetNode));
+
+        expect(result.current[1]).toMatchInlineSnapshot(`
+            <Mirror
+              frameProps={Object {}}
+              mirrorProp=""
+              reflect={<div />}
+            />
+        `);
+    });
+});
