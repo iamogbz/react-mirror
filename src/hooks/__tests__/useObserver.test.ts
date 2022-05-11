656babf472a585d9fc0dd2f22ce5baffb1a1ca46
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2022-05-10 21:07:44 -0400

    chore(deps): remove deprecated @testing-library/react-hooks package

--- src/hooks/__tests__/useObserver.test.ts
+++ src/hooks/__tests__/useObserver.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 import { useObserver, UseObserverProps } from "../useObserver";
 
 const observerMock = {
--- src/hooks/__tests__/useRenderTrigger.test.ts
+++ src/hooks/__tests__/useRenderTrigger.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react";
 import { useRenderTrigger } from "../useRenderTrigger";
 
 describe("useRenderTrigger", () => {
