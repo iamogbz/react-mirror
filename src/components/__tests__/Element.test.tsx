66f82c24a1e6fbac838abb966ecda5b55340c373
Emmanuel Ogbizi <iamogbz+github@gmail.com>
2023-07-04 13:36:09 +0000

    test: do not preserve ghost value on input reflection

--- src/components/__tests__/Element.test.tsx
+++ src/components/__tests__/Element.test.tsx
@@ -62,4 +62,26 @@ describe("Element", () => {
             </span>
         `);
   });
+
+  it("does not preserve ghost value on input element", () => {
+    const domRef = React.createRef<HTMLInputElement>();
+    const initialValue = "initial value";
+    /*
+     * Warning: You provided a `value` prop to a form field without an `onChange` handler.
+     * This will render a read-only field. If the field should be mutable use `defaultValue`.
+     * Otherwise, set either `onChange` or `readOnly`.
+     */
+    const { rerender } = render(
+      <Element domRef={domRef} tagName="input" value={initialValue} />,
+    );
+    expect(domRef.current?.value).toEqual(initialValue);
+    /*
+     * Warning: A component is changing a controlled input to be uncontrolled.
+     * This is likely caused by the value changing from a defined to undefined, which should not happen.
+     * Decide between using a controlled or uncontrolled input element for the lifetime of the component.
+     * More info: https://reactjs.org/link/controlled-components
+     */
+    rerender(<Element domRef={domRef} tagName="input" />);
+    expect(domRef.current?.value).toHaveLength(0);
+  });
 });
